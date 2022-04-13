233a294f1941b5902e8ea2be574480f3ec6cccd0
MonroeMa <me_menglu@163.com>
2022-04-13 21:02:18 +0800

    init

---
+++ precache-manifest.6f6977014ed6e581d5f8268765b37ecb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4876060ab6d45ad449b257c0f43b3dae",
+    "url": "/handAccount-H5/index.html"
+  },
+  {
+    "revision": "863b587ff5aafc76bdf6",
+    "url": "/handAccount-H5/static/css/2.7ff0d8b2.chunk.css"
+  },
+  {
+    "revision": "c87866189fa24d272397",
+    "url": "/handAccount-H5/static/css/main.361b6dea.chunk.css"
+  },
+  {
+    "revision": "863b587ff5aafc76bdf6",
+    "url": "/handAccount-H5/static/js/2.942c5c7a.chunk.js"
+  },
+  {
+    "revision": "d588a74c1ff43e1ddd18c9ac6d98a1f3",
+    "url": "/handAccount-H5/static/js/2.942c5c7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c87866189fa24d272397",
+    "url": "/handAccount-H5/static/js/main.087f5ae2.chunk.js"
+  },
+  {
+    "revision": "1e46c6473e51dbbdebe4",
+    "url": "/handAccount-H5/static/js/runtime-main.7e9d2f7e.js"
+  }
+]);
